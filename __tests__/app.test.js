5423b58a69820362e30d914740ca710688c97a93
brookeperkins <brookemmperkins@gmail.com>
2020-09-23 15:31:06 -0700

    post route for studio

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,13 +1,28 @@
 const fs = require('fs');
 const pool = require('../lib/utils/pool');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('ripe-banana routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
-  it('does something', () => {
-
+  it('returns a new studio via POST', () => {
+    return request(app)
+      .post('/api/v1/studios')
+      .send({ 
+        name: 'Warner Bros',
+        city: 'Milwaukee',
+        state: 'Florida',
+        country: 'Belgium'
+      })
+      .then(res => expect(res.body)
+        .toEqual({
+          id: expect.any(String),
+          name: 'Warner Bros',
+          city: 'Milwaukee',
+          state: 'Florida',
+          country: 'Belgium'
+        }));
   });
 });
