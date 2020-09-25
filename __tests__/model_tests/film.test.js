0d753785ba8ce2c1f823d9099609c76319e1014c
bwapes <benwaples@gmail.com>
2020-09-25 15:16:48 -0700

    film findById

--- __tests__/model_tests/film.test.js
+++ __tests__/model_tests/film.test.js
@@ -17,4 +17,29 @@ describe('Film class', () => {
     
     expect(rows[0]).toEqual({ ...insertedFilm, id: expect.any(String) });
   });
+
+  it('should find all films via GET', async() => {
+    const insertedFilm = await Film.insert({
+      title: 'Mr and Mrs Smith',
+      studio: 1,
+      released: '2008',
+      talent: [{
+        role: 'main character',
+        actor: 1
+      }]
+    });
+    const foundFilm = await Film.findById(insertedFilm.id);
+    expect(foundFilm).toEqual({
+      id: expect.any(String),
+      title: 'Mr and Mrs Smith',
+      studio: expect.any(String),
+      released: 2008,
+      talent: [{
+        role: 'main character',
+        actor: expect.any(String)
+      }]
+    });
+
+
+  });
 });
